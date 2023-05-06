8b710aecf3ee62114cca09096f3fa558b93f6595
ShenJianPing0307 <610502705@qq.com>
2023-05-06 17:11:38 +0800

    多线程案例、网络协议

--- chapter24/code/threading_queue_Queue_demo.py
+++ chapter24/code/threading_queue_Queue_demo.py
@@ -2,6 +2,43 @@ import queue
 import threading
 import time
 
-if __name__ == '__main__':
-    q = queue.Queue() # 实例化一个 先进先出 队列
 
+def do_work(item):
+    print("%s任务已经完成" % item)
+    time.sleep(0.2)
+
+
+def worker():
+    """每一个线程"""
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        # 处理任务的具体逻辑
+        do_work(item)
+        q.task_done()  # 任务完成后向队列发送一个通知信号
+
+
+if __name__ == '__main__':
+    q = queue.Queue()  # 实例化一个 先进先出 队列
+    t_list = []
+    num_worker_threads = 5
+
+    # 将10个任务放入到队列中
+    for item in range(10):
+        q.put(item)
+
+    # 5个工作线程处理任务
+    for _ in range(num_worker_threads):
+        t = threading.Thread(target=worker)
+        t.start()
+        t_list.append(t)
+
+    # 等待队列中所有任务全部取出并且进行处理
+    q.join()
+
+    # 等待所有的工作线程结束
+    for _ in range(num_worker_threads):
+        q.put(None)
+
+    [t.join() for t in t_list]
---
+++ chapter24/code/top250/douban1.py
@@ -0,0 +1,55 @@
+import requests
+import re
+import time
+import json
+from concurrent.futures import ThreadPoolExecutor
+
+def save_file(result):
+    with open("./movie/top250.txt", 'a', encoding='utf8') as f:
+        f.write(json.dumps(result, ensure_ascii=False) + '\n')
+
+
+def parse_text(page_text):
+    """
+    {"rank":1, "img":"src", "title":"天堂电影院", "guide":"朱塞佩·托纳多雷"}
+    :param text:
+    :return:
+    """
+    for item in page_text:
+        yield {
+            "rank": item[0],
+            "img": item[1],
+            "title": (item[2] + item[3]).replace("&nbsp;", '').replace(" ", ''),
+            "guide": re.sub('\s|\n', '', item[4]).split(";")[0].strip("&nbsp")
+        }
+
+
+def main(offset):
+    url = "https://movie.douban.com/top250?start=%s" % offset
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    }
+    response = requests.get(url=url, headers=headers)
+    pattern = re.compile('<em class="">(\d+)</em>.*?'
+                         + '<a.*?src="(.*?)".*?'
+                         + '<span class="title">(.*?)</span>.*?'
+                         + '<span class="other">(.*?)</span>.*?'
+                         + '<p class="">(.*?) </p>.*?',
+                         re.S
+                         )
+
+    page_text = re.findall(pattern, response.text)
+    result_iter = parse_text(page_text)
+
+    for result in result_iter:
+        save_file(result)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    pool = ThreadPoolExecutor(10)
+    for i in range(1, 11):
+        pool.submit(main, (i - 1) * 25)
+    pool.shutdown()
+    end_time = time.time()
+    print(end_time-start_time) # 0.9797060489654541
